e0d6a96ebb9e4aaf69ee1d1b067d139c027aedbb
yin753 <101307346+yin753@users.noreply.github.com>
2025-09-27 23:46:27 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,4 +15,4 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git mosdns src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5' >>feeds.conf.default
+echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns.git;v5' >>feeds.conf.default
